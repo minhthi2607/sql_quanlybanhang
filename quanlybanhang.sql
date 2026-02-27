ab9419a0190837c3db56285c0032f1764afa5d53
Phan Minh Thi <thi@corsivalab.space>
2026-02-27 20:58:50 +0700

    update

--- quanlybanhang.sql
+++ quanlybanhang.sql
@@ -12,14 +12,14 @@ create table orders(
 	oId int not null auto_increment primary key,
     cId int not null,
     oDate datetime,
-    oTotalPrice double not null,
+    oTotalPrice int,
     foreign key (cId) references customers(cId)
-);
+);	
 
 create table products(
 	pId int not null auto_increment primary key,
     pName varchar(255) not null,
-    pPrice double not null
+    pPrice int 
 );
 
 create table orderDetails(
@@ -31,3 +31,58 @@ create table orderDetails(
     foreign key (pId) references products(pId)
     
 );
+
+insert into customers value(1, 'minh quan', 10), 
+(2, 'ngoc anh', 20),
+(3, 'hong ha', 50);
+
+select * from customers;
+
+INSERT INTO orders (oId, cId, oDate) VALUES
+(1, 1, '2006-03-21'),
+(2, 2, '2006-03-23'),
+(3, 1, '2006-03-16');
+
+select * from orders;
+
+INSERT INTO products (pId, pName, pPrice) VALUES
+(1, 'May Giat', 3),
+(2, 'Tu Lanh', 5),
+(3, 'Dieu Hoa', 7),
+(4, 'Quat', 1),
+(5, 'Bep Dien', 2);
+
+select * from products;
+
+INSERT INTO orderDetails (oId, pId, odQTY) VALUES
+(1, 1, 3),
+(1, 3, 7),
+(1, 4, 2),
+(2, 1, 1),
+(3, 1, 8),
+(2, 5, 4),
+(2, 3, 3);
+
+select * from orderDetails;
+
+select oId, oDate, oTotalPrice from orders;
+
+select DISTINCT  c.cName , p.pName
+from customers c
+inner join orders o on c.cId = o.cId
+inner join orderDetails od on o.oId = od.oId
+inner join products p on od.pId = p.pId;
+
+select c.cName
+from customers c
+left join orders o on c.cId = o.cId
+where o.cId is null;
+
+select o.oId, o.oDate, 
+sum(od.odQTY * p.pPrice) as totalPrice
+from orders o
+inner join orderDetails od on o.oId = od.oId
+inner join products p on od.pId = p.pId
+group by o.oId, o.oDate;
+
+ 
